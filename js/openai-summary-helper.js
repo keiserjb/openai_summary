bb8076996c7a15d095c2fe7ca4e60931f85e2d7b
keiserjb <keiserjb@yahoo.com>
2023-11-12 11:13:30 -0500

    ajax changes

--- js/openai-summary-helper.js
+++ js/openai-summary-helper.js
@@ -1,57 +1,40 @@
-(function ($, Drupal) {
-  Drupal.behaviors.openaiSummaryHelper = {
-    attach: function (context) {
-      once(
-        "openai-summary-helper",
-        '[data-drupal-selector="edit-body-wrapper"]',
-        context
-      ).forEach((el) => {
+(function ($) {
+  Backdrop.behaviors.openaiSummaryHelper = {
+    attach: function (context, settings) {
+      $('.openai-summary-btn', context).once('openai-summary-helper').each(function () {
 
-        const showThrobber = () => {
-          const wrapper = document.querySelector(
-            `[data-drupal-selector="edit-body-wrapper"]`
-          );
-          const existingThrobber = document.querySelector(".ajax-progress");
-          if (existingThrobber) {
-            wrapper.parentElement.appendChild(existingThrobber);
+        var showThrobber = function() {
+          var wrapper = $('#edit-body');
+          var existingThrobber = $('.ajax-progress');
+          if (existingThrobber.length) {
+            wrapper.parent().append(existingThrobber);
           } else {
-            const throbber = document.createElement("div");
-            throbber.classList.add("ajax-progress", "ajax-progress--throbber");
-            throbber.innerHTML = `
-              <div class="ajax-progress__throbber">
-              </div>
-              <div class="ajax-progress__message">
-              Please wait...</div>`;
-            wrapper.parentElement.appendChild(throbber);
+            var throbber = $('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
+            wrapper.parent().append(throbber);
           }
         };
 
-        const removeThrobber = () => {
-          const throbberElements = document.querySelectorAll(".ajax-progress");
-          throbberElements.forEach((element) => {
-            element.remove();
-          });
+        var removeThrobber = function() {
+          $('.ajax-progress').remove();
         };
 
-        const simulateClick = (element) => {
-          const event = new MouseEvent("click", {
+        var simulateClick = function(element) {
+          var e = new MouseEvent('click', {
             bubbles: true,
             cancelable: true,
-            view: window,
+            view: window
           });
-          element.dispatchEvent(event);
+          element.dispatchEvent(e);
           showThrobber();
         };
 
         removeThrobber();
 
-        const buttonElement = document.querySelector(".openai-summary-btn");
-        const submitBtn = document.querySelector(
-          '[data-drupal-selector="edit-fill-summary"]'
-        );
-        submitBtn.classList.add("visually-hidden");
-        buttonElement.addEventListener("click", () => {
-          simulateClick(submitBtn);
+        var buttonElement = $(this);
+        var submitBtn = $('#edit-fill-summary');
+        submitBtn.addClass('visually-hidden');
+        buttonElement.click(function() {
+          simulateClick(submitBtn[0]);
         });
 
         $(document).ajaxError(function () {
@@ -60,12 +43,8 @@
 
         $(document).ajaxComplete(function () {
           removeThrobber();
-          const buttonElement = document.querySelector(".openai-summary-btn");
-          buttonElement.addEventListener("click", () => {
-            simulateClick(submitBtn);
-          });
         });
       });
-    },
+    }
   };
-})(jQuery, Drupal);
+})(jQuery);
